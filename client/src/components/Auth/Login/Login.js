834bcdeed9a538312b5e80517f718d5c76e7a42a
nicolasraffray <nraffray@gmail.com>
2020-06-16 16:52:30 +0100

    links put in

--- client/src/components/Auth/Login/Login.js
+++ client/src/components/Auth/Login/Login.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div>
       <form className="d-flex mt-5">
         <input type="text" value=""></input>
-        <button>Login</button>
+        <Link to="/">
+          <button>Login</button>
+        </Link>
       </form>
     </div>
   );
