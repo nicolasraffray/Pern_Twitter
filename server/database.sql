d926a2648ba9919f2d8b0134ddca370ff1682c80
nicolasraffray <nraffray@gmail.com>
2020-06-17 15:37:26 +0100

    user table added

--- server/database.sql
+++ server/database.sql
@@ -7,3 +7,24 @@ CREATE TABLE Posts
   Post varchar(280)
 );
 
+CREATE TABLE Users
+(
+  UserID SERIAL Primary Key,
+  Email varchar(255) ,
+  UserName varchar(255) ,
+  password varchar(255),
+  UNIQUE (Email),
+  UNIQUE(UserName)
+);
+
+ALTER TABLE Posts
+DROP COLUMN UserName;
+
+ALTER TABLE Posts
+ADD COLUMN UserID int;
+
+ALTER TABLE Posts
+ADD CONSTRAINT constraint_fk FOREIGN KEY (UserId) REFERENCES Users
+(userid)
+ON
+DELETE CASCADE;
