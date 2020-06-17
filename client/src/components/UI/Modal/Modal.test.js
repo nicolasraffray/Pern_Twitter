2fcd56a064ad597d4b58cae3352f70c6c3c68204
nicolasraffray <nraffray@gmail.com>
2020-06-17 13:48:09 +0100

    modal passing test

--- client/src/components/UI/Modal/Modal.test.js
+++ client/src/components/UI/Modal/Modal.test.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { shallow } from "enzyme";
 import Modal from "./Modal";
 
 describe("Post", () => {
   it("should render a <div />", () => {
-    const wrapper = shallow(<Modal />);
-    expect(wrapper.find(<Fragment />).length).toEqual(1);
+    const wrapper = shallow(<Modal post={{ post: "fake post" }} key={1} />);
+    console.log(wrapper.find("Fragment"));
+    expect(wrapper.find("Fragment").length).toEqual(1);
   });
 });
