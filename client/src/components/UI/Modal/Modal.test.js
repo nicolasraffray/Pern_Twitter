c92a1caa447c5cb748fc16f622d993f22d7a2b70
nicolasraffray <nraffray@gmail.com>
2020-06-17 13:57:01 +0100

    modal testing for buttons

--- client/src/components/UI/Modal/Modal.test.js
+++ client/src/components/UI/Modal/Modal.test.js
@@ -5,7 +5,11 @@ import Modal from "./Modal";
 describe("Post", () => {
   it("should render a <div />", () => {
     const wrapper = shallow(<Modal post={{ post: "fake post" }} key={1} />);
-    console.log(wrapper.find("Fragment"));
     expect(wrapper.find("Fragment").length).toEqual(1);
+  });
+
+  it("should render buttons also", () => {
+    const wrapper = shallow(<Modal post={{ post: "fake post" }} key={1} />);
+    expect(wrapper.find("button").length).toEqual(4);
   });
 });
