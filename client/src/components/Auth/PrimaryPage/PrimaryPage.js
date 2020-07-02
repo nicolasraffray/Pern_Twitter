fc588061cbe04d0d62cbc1d55deace110f463453
nicolasraffray <nraffray@gmail.com>
2020-07-02 18:10:35 +0100

    primary page css complete

--- client/src/components/Auth/PrimaryPage/PrimaryPage.js
+++ client/src/components/Auth/PrimaryPage/PrimaryPage.js
@@ -4,13 +4,18 @@ import classes from "./PrimaryPage.module.css";
 
 const PrimaryPage = () => {
   return (
-    <div className="Primary">
-      <Link to="/login">
-        <button className="btn btn-outline-success">Login</button>
-      </Link>
-      <Link to="/signup">
-        <button className="btn btn-outline-success">Sign Up</button>
-      </Link>
+    <div className={classes.Primary}>
+      <div className={classes.Welcome}>
+        <h1>Welcome To Twitter!</h1>
+      </div>
+      <div className={classes.Links}>
+        <Link to="/login">
+          <button className="btn btn-outline-success m-3">Login</button>
+        </Link>
+        <Link to="/signup">
+          <button className="btn btn-outline-success m-3">Sign Up</button>
+        </Link>
+      </div>
     </div>
   );
 };
